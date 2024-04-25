19e513b66bb94152c73ed62dcecb440029c90f1c
c3nsored <u.gabrielr@gmail.com>
2024-04-25 22:33:48 +0200

    fetch links from a url

---
+++ fetcher.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net/http"
+
+	"golang.org/x/net/html"
+)
+
+type Fetcher interface {
+	Fetch(url string) ([]string, error)
+}
+
+type fetch struct {
+	fetched map[string]bool
+}
+
+func (f fetch) Fetch(url string) ([]string, error) {
+	urls := []string{}
+
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	doc, err := html.Parse(bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+
+	var fn func(*html.Node)
+	fn = func(h *html.Node) {
+		if h.Type == html.ElementNode && h.Data == "a" {
+			for _, a := range h.Attr {
+				if a.Key == "href" {
+					urls = append(urls, a.Val)
+				}
+			}
+		}
+
+		for c := h.FirstChild; c != nil; c = c.NextSibling {
+			fn(c)
+		}
+	}
+
+	fn(doc)
+	if len(urls) <= 0 {
+		log.Printf("found zero urls\n")
+	}
+
+	return urls, nil
+}
